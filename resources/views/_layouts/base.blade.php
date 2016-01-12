62df6e2d9c3b8c37bd4bd817472b773583edabd4
Muhammad Alkaff <m.alkaff9@gmail.com>
2016-01-12 10:01:35 +0800

    perbaikan tampilan

--- resources/views/_layouts/base.blade.php
+++ resources/views/_layouts/base.blade.php
@@ -59,7 +59,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="{{ url('/') }}">Teknik Informatika</a>
+      <a class="navbar-brand" href="{{ url('/') }}">Fakultas Teknik</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
