69e98f6cce64fdf617761706d7405b0680128e73
alkzzz <m.alkaff9@gmail.com>
2016-02-26 07:28:39 +0800

    add footer

--- resources/views/_layouts/base.blade.php
+++ resources/views/_layouts/base.blade.php
@@ -84,57 +84,22 @@
     </div><!-- /.navbar-collapse -->
 </nav>
 
-<div id="imageSlider" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    @foreach($sliders as $key => $value)
-    <li data-target="#imageSlider" data-slide-to="{{ $key }}" @if($key == 0) class="active"> @endif </li>
-    @endforeach
-  </ol>
+@yield('slider')
 
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    @foreach($sliders as $key => $feature)
-    @if($key == 0)
-    <div class="item active">
-    @else
-    <div class="item">
-    @endif
-    <img src="{{ $feature->slider->gambar }}" @if(Localization::getCurrentLocale() == 'id') alt="{{ $feature->title_id }}" @else alt="{{ $feature->title_en }}" @endif>
-    <div class="carousel-caption">
-      @if(Localization::getCurrentLocale() == 'id')
-      <h3>{{ $feature->title_id }}</h3>
-      <p>{{ $feature->content_id }}</p>
-      @else
-      <h3>{{ $feature->title_en }}</h3>
-      <p>{{ $feature->content_en }}</p>
-      @endif
-    </div>
-    </div>
-    @endforeach
-  </div>
-
-  <!-- Left and right controls -->
-  <a class="left carousel-control" href="#imageSlider" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#imageSlider" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-
-</div>
-
-<div class="container">
+<div class="container" style="height:240px">
     @yield('content')
     <!-- Content -->
 </div>
 
-    @section('js')
-          <script src="{{ asset('js/frontend.js') }}"></script>
-    @show
+<div class="container">
+<footer class="footer" style="background-color:grey;height:200px">
+      <p>Place sticky footer content here.</p>
+</footer>
+</div>
+
+@section('js')
+      <script src="{{ asset('js/frontend.js') }}"></script>
+@show
 
   </body>
 </html>
