b4a1010defe0155ac1eb3901c7ff57d2a8388ced
alkzzz <m.alkaff9@gmail.com>
2016-03-29 12:08:15 +0800

    add link dosen

--- resources/views/dosen/daftarDosen.blade.php
+++ resources/views/dosen/daftarDosen.blade.php
@@ -3,16 +3,76 @@
 @section('title', $title)
 
 @section('content')
-  <label>Link</label>
+  <label>Link ID</label>
   <br>
-  <input disabled type="text" class="form-control" style="width:30%;display:inline-block;font-weight:bold" id="copyTarget" value="{{ route('dosen.index') }}">
-  <button class="btn btn-info" id="copyButton"> Copy Link</button>
+  <input disabled type="text" class="form-control" style="width:30%;display:inline-block;font-weight:bold" id="link_ID" value="{{ Localization::getURLFromRouteNameTranslated('id', 'trans.dosen') }}">
+  <button class="btn btn-info" id="copyLinkID"> Copy Link</button>
+  <br>
+  <label>Link EN</label>
+  <br>
+  <input disabled type="text" class="form-control" style="width:30%;display:inline-block;font-weight:bold" id="link_EN" value="{{ Localization::getURLFromRouteNameTranslated('en', 'trans.dosen') }}">
+  <button class="btn btn-info" id="copyLinkEN"> Copy Link</button>
 @stop
 @section('js')
 @parent
 <script type="text/javascript">
-document.getElementById("copyButton").addEventListener("click", function() {
-  copyToClipboard(document.getElementById("copyTarget"));
+document.getElementById("copyLinkID").addEventListener("click", function() {
+  copyToClipboard(document.getElementById("link_ID"));
+});
+
+function copyToClipboard(elem) {
+  // create hidden text element, if it doesn't already exist
+  var targetId = "_hiddenCopyText_";
+  var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
+  var origSelectionStart, origSelectionEnd;
+  if (isInput) {
+      // can just use the original source element for the selection and copy
+      target = elem;
+      origSelectionStart = elem.selectionStart;
+      origSelectionEnd = elem.selectionEnd;
+  } else {
+      // must use a temporary form element for the selection and copy
+      target = document.getElementById(targetId);
+      if (!target) {
+          var target = document.createElement("textarea");
+          target.style.position = "absolute";
+          target.style.left = "-9999px";
+          target.style.top = "0";
+          target.id = targetId;
+          document.body.appendChild(target);
+      }
+      target.textContent = elem.textContent;
+  }
+  // select the content
+  var currentFocus = document.activeElement;
+  target.focus();
+  target.setSelectionRange(0, target.value.length);
+
+  // copy the selection
+  var succeed;
+  try {
+      succeed = document.execCommand("copy");
+  } catch(e) {
+      succeed = false;
+  }
+  // restore original focus
+  if (currentFocus && typeof currentFocus.focus === "function") {
+      currentFocus.focus();
+  }
+
+  if (isInput) {
+      // restore prior selection
+      elem.setSelectionRange(origSelectionStart, origSelectionEnd);
+  } else {
+      // clear temporary content
+      target.textContent = "";
+  }
+  return succeed;
+}
+</script>
+<script type="text/javascript">
+document.getElementById("copyLinkEN").addEventListener("click", function() {
+  copyToClipboard(document.getElementById("link_EN"));
 });
 
 function copyToClipboard(elem) {
