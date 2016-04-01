8b5754116b3448dda84290069ed7221030846505
alkzzz <m.alkaff9@gmail.com>
2016-04-01 16:05:03 +0800

    change footer

--- resources/views/_layouts/base.blade.php
+++ resources/views/_layouts/base.blade.php
@@ -111,54 +111,45 @@
     </div>
 </div>
 
-<footer class="footer-distributed">
-  <div class="footer-left">
-    <h3>Company<span>logo</span></h3>
-    <p class="footer-links">
-      <a href="#">Home</a>
-      ·
-      <a href="#">Blog</a>
-      ·
-      <a href="#">Pricing</a>
-      ·
-      <a href="#">About</a>
-      ·
-      <a href="#">Faq</a>
-      ·
-      <a href="#">Contact</a>
-    </p>
-    <p class="footer-company-name">Company Name &copy; 2015</p>
-  </div>
-  <div class="footer-center">
-    <div>
-      <i class="fa fa-map-marker"></i>
-      <p><span>21 Revolution Street</span> Paris, France</p>
+<footer>
+  <div class="container-fluid">
+    <div class="row footer-link">
+      <div class="col-md-4">
+        <p>
+          footer 1
+        </p>
+      </div>
+      <div class="col-md-4">
+        <p>
+          footer 2
+        </p>
+      </div>
+      <div class="col-md-4">
+        <p>
+          footer 3
+        </p>
+      </div>
     </div>
-    <div>
-      <i class="fa fa-phone"></i>
-      <p>+1 555 123456</p>
-    </div>
-    <div>
-      <i class="fa fa-envelope"></i>
-      <p><a href="mailto:support@company.com">support@company.com</a></p>
-    </div>
-  </div>
-  <div class="footer-right">
-    <p class="footer-company-about">
-      <span>About the company</span>
-      Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
-    </p>
-    <div class="footer-icons">
-      <a href="#"><i class="fa fa-facebook"></i></a>
-      <a href="#"><i class="fa fa-twitter"></i></a>
-      <a href="#"><i class="fa fa-linkedin"></i></a>
-      <a href="#"><i class="fa fa-github"></i></a>
+    <div class="row copyright">
+      <div class="col-md-12">
+        <p>
+          Copyright<i class="fa fa-copyright"></i> Fakultas Teknik 2016
+        </p>
+          <button class="btn btn-primary pull-right" type="button" id="btnTop"><i class="fa fa-arrow-up fa-lg fa-fw"></i></button>
+      </div>
     </div>
   </div>
 </footer>
 
 @section('js')
       <script src="{{ asset('js/frontend.js') }}"></script>
+      <script type="text/javascript">
+        $(document).ready(function() {
+          $('#btnTop').click(function(event) {
+            $("html, body").animate({ scrollTop: "0" }, 'slow');
+          });
+        });
+      </script>
 @show
 
   </body>
